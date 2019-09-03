7f8802ef1b048db0b3bf6c5fb1ee0c7fdd559576
Gavrie Philipson <gavrie@redislabs.com>
2019-09-03 14:22:08 +0300

    Build libredisearch.a dependency

--- build.rs
+++ build.rs
@@ -1,7 +1,6 @@
 extern crate bindgen;
-extern crate cc;
 
-use std::env;
+use cmake::Config;
 
 fn main() {
     // Generate bindings for RediSearch
@@ -21,21 +20,15 @@ fn main() {
         .expect("failed to write RediSearch bindings to file");
 
     // Find and link statically to libredisearch.a
+    // TODO: Consider splitting the low level libredisearch wrapper off into a separate `-sys` crate.
 
-    // TODO: Instead of relying on a pre-built library,
-    // we should build RediSearch as a static lib, like this:
-    //
-    //    mkdir -p build
-    //    cd build
-    //    cmake -DRS_BUILD_STATIC=ON ..
-    //    make
-    //
-    // Take a look at some `-sys` crates for system library wrappers, and build using the
-    // same methods that they use.
-    // In fact, consider splitting this crate into a `-sys` crate for the low level wrappers
-    // and another one for the high-level bindings.
+    let mut dst = Config::new("RediSearch")
+        .define("RS_BUILD_STATIC", "ON")
+        .build_target("redisearchS")
+        .build();
 
-    let lib_search_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-    println!("cargo:rustc-link-search=native={}", lib_search_dir);
+    dst.push("build");
+
+    println!("cargo:rustc-link-search=native={}", dst.display());
     println!("cargo:rustc-link-lib=static=redisearch");
 }
